d89d383b5c9b997161b0d529f2ef754b04faac29
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-03-12 12:13:22 +0530

    added headers to avoid CORS errors

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if(req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Method','GET,POST,PUT,DELETE,PATCH,');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 
