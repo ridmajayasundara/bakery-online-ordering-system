caf41531024207be25f885ec431bbdafc62da215
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-03-15 23:00:11 +0530

    added POST request to /orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const route = express.Router();
 
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+
 
 route.get('/',(req,res,next)=>{
     res.status(200).json({
@@ -9,10 +12,25 @@ route.get('/',(req,res,next)=>{
 })
 
 route.post('/',(req,res,next)=>{
-    const order = {
-        orderId : req.body.orderId,
+    const order = new Order({
+        _id : mongoose.Types.ObjectId(),
+        product : req.body.productId,
         quantity : req.body.quantity
-    };
+
+    })
+    order.save()
+    then(result=>{
+        res.status(201).json({
+            message : 'order stored successfully!',
+            order_object : result
+        })
+    })
+    .catch(error=>{
+        res.status(500).json({
+            message : "server error when trying to store order",
+            error : error
+        })
+    })
     res.status(201).json({
         Message : "order post request",
         orderId : orderId
